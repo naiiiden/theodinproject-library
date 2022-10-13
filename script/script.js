53065241f277ee823e3f537085be329f3cd14aa1
naiiiden <na4ko359@gmail.com>
2022-10-13 18:13:52 +0300

    add myLibrary array with 4 manually added books so i can see the display

--- script/script.js
+++ script/script.js
@@ -1,1 +1,26 @@
-console.log(1);
+let myLibrary = [
+    {
+        bookTitle: "Warcraft: Of Blood and Honor",
+        bookAuthor: "Chris Metzen",
+        bookPages: 128,
+        read: false,
+    },
+    {
+        bookTitle: "Warcraft: Day of the Dragon",
+        bookAuthor: "Richard A. Knaak",
+        bookPages: 384,
+        read: false,
+    },
+    {
+        bookTitle: "Warcraft: Lord of the Clans",
+        bookAuthor: "Christie Golden",
+        bookPages: 278,
+        read: false,
+    },
+    {
+        bookTitle: "Warcraft: The Last Guardian",
+        bookAuthor: "Jeff Grubb",
+        bookPages: 308,
+        read: false,
+    },
+];
