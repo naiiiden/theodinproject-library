510afdc80838a1757562a7523f123c06ee78afd3
naiiiden <na4ko359@gmail.com>
2022-10-16 19:43:05 +0300

    form submit now works properly

--- script/script.js
+++ script/script.js
@@ -1,15 +1,16 @@
 let myLibrary = [];
 
-for (let i = 0; i < myLibrary.length; i++) {
-    document.querySelector("#main").innerHTML += 
-    `<div class="book-container" data-id=${i}>
-    <button class="delete" aria-label="Remove book from library" title="Remove book from library">X</button>
-    <h2>Title: <span>${myLibrary[i].bookTitle}</span></h2>
-    <p>Author: <span>${myLibrary[i].bookAuthor}</span></p>
-    <p>Pages: <span>${myLibrary[i].bookPages}</span></p>
-    <p>Read: <span>${myLibrary[i].read ? "Yes" : "No"}</span></p>
-    </div>`;
-};
+//print pre-written books for better styling
+// for (let i = 0; i < myLibrary.length; i++) {
+//     document.querySelector("#main").innerHTML += 
+//     `<div class="book-container" data-id=${i}>
+//     <button class="delete" aria-label="Remove book from library" title="Remove book from library">X</button>
+//     <h2>Title: <span>${myLibrary[i].bookTitle}</span></h2>
+//     <p>Author: <span>${myLibrary[i].bookAuthor}</span></p>
+//     <p>Pages: <span>${myLibrary[i].bookPages}</span></p>
+//     <p>Read: <span>${myLibrary[i].read ? "Yes" : "No"}</span></p>
+//     </div>`;
+// };
 
 function Book(title, author, pages, read) {
     //constructor
@@ -19,7 +20,7 @@ function Book(title, author, pages, read) {
     this.read = read;
 }
 
-function addBookToLibrary(title, author, pages, read) {
+function addBookToLibrary() {
     myLibrary.push(new Book (
         title = document.querySelector("#title-input").value,
         author = document.querySelector("#author-input").value,
@@ -29,7 +30,7 @@ function addBookToLibrary(title, author, pages, read) {
 }
 
 function displayBook() {
-    document.querySelector("#main").innerHTML += 
+    document.querySelector(".cards-container").innerHTML += 
     `<div class="book-container" data-id=${myLibrary.length - 1}>
     <button class="delete" aria-label="Remove book from library" title="Remove book from library">X</button>
     <h2>Title: <span>${myLibrary[myLibrary.length - 1].bookTitle}</span></h2>
