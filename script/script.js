0a33e408b7100e3e9eea4f7fe696acf214d18ceb
naiiiden <na4ko359@gmail.com>
2022-10-16 20:21:11 +0300

    add pre-written books

--- script/script.js
+++ script/script.js
@@ -1,16 +1,41 @@
-let myLibrary = [];
+let myLibrary = [
+    {
+        bookTitle: "Warcraft: Of Blood and Honor",
+        bookAuthor: "Chris Metzen",
+        bookPages: 128,
+        read: false,
+    },
+    {
+        bookTitle: "Warcraft: Day of the Dragon",
+        bookAuthor: "Richard A. Knaak",
+        bookPages: 384,
+        read: false,
+    },
+    {
+        bookTitle: "Warcraft: Lord of the Clans",
+        bookAuthor: "Christie Golden",
+        bookPages: 278,
+        read: false,
+    },
+    {
+        bookTitle: "Warcraft: The Last Guardian",
+        bookAuthor: "Jeff Grubb",
+        bookPages: 308,
+        read: false,
+    },
+];
 
-//print pre-written books for better styling
-// for (let i = 0; i < myLibrary.length; i++) {
-//     document.querySelector("#main").innerHTML += 
-//     `<div class="book-container" data-id=${i}>
-//     <button class="delete" aria-label="Remove book from library" title="Remove book from library">X</button>
-//     <h2>Title: <span>${myLibrary[i].bookTitle}</span></h2>
-//     <p>Author: <span>${myLibrary[i].bookAuthor}</span></p>
-//     <p>Pages: <span>${myLibrary[i].bookPages}</span></p>
-//     <p>Read: <span>${myLibrary[i].read ? "Yes" : "No"}</span></p>
-//     </div>`;
-// };
+// print pre-written books for better styling
+for (let i = 0; i < myLibrary.length; i++) {
+    document.querySelector(".cards-container").innerHTML += 
+    `<div class="book-container" data-id=${i}>
+    <button class="delete" aria-label="Remove book from library" title="Remove book from library">X</button>
+    <h2>Title: <span>${myLibrary[i].bookTitle}</span></h2>
+    <p>Author: <span>${myLibrary[i].bookAuthor}</span></p>
+    <p>Pages: <span>${myLibrary[i].bookPages}</span></p>
+    <p>Read: <span>${myLibrary[i].read ? "Yes" : "No"}</span></p>
+    </div>`;
+};
 
 function Book(title, author, pages, read) {
     //constructor
