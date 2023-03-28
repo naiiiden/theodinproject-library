76224df490f6ec9cfef94ec9a128afdb749321c4
naiiiden <na4ko359@gmail.com>
2023-03-28 19:31:47 +0300

    book assign unique ids

--- script/script.js
+++ script/script.js
@@ -1,14 +1,15 @@
 let myLibrary = [];
+let bookId = 0;
 
-myLibrary.push(new Book("Warcraft: Of Blood and Honor", "Chris Metzen", 128, false));
-myLibrary.push(new Book("Warcraft: Day of the Dragon", "Richard A. Knaak", 384, false));
-myLibrary.push(new Book("Warcraft: Lord of the Clans", "Christie Golden", 278, false));
-myLibrary.push(new Book("Warcraft: The Last Guardian", "Jeff Grubb", 308, false));
+myLibrary.push(new Book("Warcraft: Of Blood and Honor", "Chris Metzen", 128, false, bookId++));
+myLibrary.push(new Book("Warcraft: Day of the Dragon", "Richard A. Knaak", 384, false, bookId++));
+myLibrary.push(new Book("Warcraft: Lord of the Clans", "Christie Golden", 278, false, bookId++));
+myLibrary.push(new Book("Warcraft: The Last Guardian", "Jeff Grubb", 308, false, bookId++));
 
 for (let i = 0; i < myLibrary.length; i++) {
     const bookContainer =
     `<div class="book-container">
-        <button class="delete" aria-label="Remove ${myLibrary[i].title} from library" title="Remove book from library">X</button>
+        <button class="delete" aria-label="Remove ${myLibrary[i].title} from library" title="Remove book from library" data-id="${myLibrary[i].id}">X</button>
         <h2>Title: <span>${myLibrary[i].title}</span></h2>
         <p>Author: <span>${myLibrary[i].author}</span></p>
         <p>Pages: <span>${myLibrary[i].pages}</span></p>
@@ -18,11 +19,12 @@ for (let i = 0; i < myLibrary.length; i++) {
     document.querySelector(".cards-container").insertAdjacentHTML("beforeend", bookContainer);
 };
 
-function Book(title, author, pages, read) {
+function Book(title, author, pages, read, id) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
+    this.id = id;
 }
 
 Book.prototype.changeRead = function() {
@@ -36,10 +38,11 @@ function addBookToLibrary() {
         author = document.querySelector("#author-input").value,
         pages = document.querySelector("#pages-input").value,
         document.querySelector("#yes-input").checked == true ? read = true : read = false,
+        bookId++,
     ));
     const bookContainer =
     `<div class="book-container">
-        <button class="delete" aria-label="Remove ${myLibrary[myLibrary.length - 1].title} from library" title="Remove book from library">X</button>
+        <button class="delete" aria-label="Remove ${myLibrary[myLibrary.length - 1].title} from library" title="Remove book from library" data-id="${myLibrary[myLibrary.length - 1].id}">X</button>
         <h2>Title: <span>${myLibrary[myLibrary.length - 1].title}</span></h2>
         <p>Author: <span>${myLibrary[myLibrary.length - 1].author}</span></p>
         <p>Pages: <span>${myLibrary[myLibrary.length - 1].pages}</span></p>
