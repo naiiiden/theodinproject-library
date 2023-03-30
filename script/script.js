51d187a6498764046e8757b396ed2c602db68b8a
naiiiden <na4ko359@gmail.com>
2023-03-30 11:00:43 +0300

    trap focus when form modal is open

--- script/script.js
+++ script/script.js
@@ -83,3 +83,63 @@ document.querySelector(".cards-container").addEventListener("click", (e) => {
         }
     }
 });
+
+function createFocusTrap(modalElement, openModalButtons) {
+    let openModal = false;
+    let previousFocusElement = null;
+
+    const toggleModal = () => {
+        openModal = !openModal;
+        if (openModal) {
+        previousFocusElement = document.activeElement;
+        }
+    };
+
+    openModalButtons.forEach((btn) => {
+        btn.addEventListener("click", () => {
+            toggleModal();
+            modalElement.style.display = openModal ? "block" : "none";
+            focusTrap(modalElement);
+        });
+    });
+
+    function focusTrap(modalElement) {
+        const focusableElements = "a[href]:not([disabled]), button:not([disabled]), textarea:not([disabled]), input:not([disabled]):not([type='hidden']):not([type='radio']):not([type='checkbox']), select:not([disabled]), [contenteditable]:not([disabled]), [tabindex]:not([tabindex='-1'])";
+        const firstFocusableElement = modalElement.querySelectorAll(focusableElements)[0];
+        const focusableContent = modalElement.querySelectorAll(focusableElements);
+        const lastFocusableElement = focusableContent[focusableContent.length - 1];
+
+        document.addEventListener("keydown", (e) => {
+            let isTabPressed = e.key === "Tab" || e.keyCode === 9;
+
+            if ((e.key === "Escape" || e.keyCode === 27) && openModal) {
+                toggleModal(); 
+                modalElement.style.display = "none";
+                // little bug here: line 119, 1220 fix this
+                document.querySelector(".form-overlay").classList.toggle("active");
+                document.querySelector(".form-container").classList.toggle("show");
+                previousFocusElement.focus(); 
+            }
+
+            if (!isTabPressed) {
+                return;
+            }
+
+            if (e.shiftKey) {
+                if (document.activeElement === firstFocusableElement) {
+                    lastFocusableElement.focus();
+                    e.preventDefault();
+                }
+            } else {
+                if (document.activeElement === lastFocusableElement) {
+                    firstFocusableElement.focus();
+                    e.preventDefault();
+                }
+            }
+        });
+        firstFocusableElement.focus();
+    }
+focusTrap(modalElement);
+}
+
+createFocusTrap(document.querySelector('.form-container'), document.querySelectorAll('.open-close-button'));
